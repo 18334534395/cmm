506be84011d82955404fc2fb6ab860296279146b
cmm <810951466@qq.com>
2020-07-07 11:17:55 +0800

    say hi

--- src/main/java/com.itheima.hdfsClient/ClientMy.java
+++ src/main/java/com.itheima.hdfsClient/ClientMy.java
@@ -8,10 +8,11 @@ import java.net.URI;
 
 public class ClientMy {
     public static void main(String[] args) throws Exception{
-        FileSystem fileSystem =FileSystem.get(new URI("hdfs://node0:8020"),new Configuration(),"root");
+       /* FileSystem fileSystem =FileSystem.get(new URI("hdfs://node0:8020"),new Configuration(),"root");
         fileSystem.create(new Path("/cmm/wmm"),true);
         fileSystem.copyFromLocalFile(new Path("D:\\java1.txt"),new Path("/cmm"));
         fileSystem.copyToLocalFile(new Path("/cmm/java1.txt"),new Path("E:\\java.txt"));
-        fileSystem.close();
+        fileSystem.close();*/
+        System.out.println("hi jds");
     }
 }
